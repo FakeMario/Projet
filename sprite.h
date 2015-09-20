fb24981f22f279143aea2497c78f2576f366c614
Lucas <lucas@lucas-laptop.(none)>
2015-09-20 15:34:10 +0200

    20sept déplacement background

---
+++ sprite.h
@@ -0,0 +1,22 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "math.h"
+#include <unistd.h>
+#include "SDL.h"
+#include <time.h>
+
+
+ struct s_sprite {
+   SDL_Surface *sprite; 
+   int colorkey; /* color which has to be invisible on the background */
+   SDL_Rect rc_image; /*coords on the bmp */
+   SDL_Rect coord; /* coords on the screen */
+   const int size; /* sprite size */
+};
+typedef struct s_sprite *pt_sprite;
+
+
+#endif
