5455dc40d2944dddd447b9eb49a8b6e3c2cd1c2d
Lucas Vignali <vignali2u@fst-ats-309-l18.fst.site.univ-lorraine.fr>
2015-10-02 10:29:51 +0200

    fromage

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 
 #include "SDL.h"
 #include "sprite.cpp"
-#include "enemy.h"
+#include "enemy.cpp"
+#include "tiles.cpp"
 
+#define TAILLE_TUILE 64
+#define NB_BLOCS_HAUTEUR 12
+#define NB_BLOCS_LARGEUR 16
 #define SCREEN_WIDTH  640
 #define SCREEN_HEIGHT 480
 #define SPRITE_WIDTH  50
@@ -16,7 +20,6 @@ int level = 1;
 int levelover = 0;
 
 /* source and destination rectangles */
-SDL_Rect camera;
 pt_sprite hero = (pt_sprite) malloc (sizeof(s_sprite));
 
 void update_events(char*keys);
@@ -62,9 +65,6 @@ void HandleEvent(char* key, SDL_Surface *screen)
 	hero->rc_image.x = 2*SPRITE_WIDTH;
       else
 	hero->rc_image.x += SPRITE_WIDTH;
-      camera.x -= CAMERA_SPEED;
-      if (camera.x <= 0)
-	camera.x = 2000-640;
     }
 
     if(key[tabkey[i][2]]) { //RIGHT
@@ -74,147 +74,139 @@ void HandleEvent(char* key, SDL_Surface *screen)
 	hero->rc_image.x = 0;
       else
 	hero->rc_image.x += SPRITE_WIDTH;
-
-      camera.x += CAMERA_SPEED;
     }
   }
 }	       
 
 int main(int argc, char* argv[])
 {
-  SDL_Surface *screen, *temp, *background, *tile0, *tile1, *tile2, *tile3;
-  SDL_Rect rcBackground;
-  enemy_type robot_enemy;
-  list_of_enemy enemy_list;
+  SDL_Surface *screen, *temp, *background, *tile0, *tile1, *tile2, *tile3, *tileset;
+  enemy_type robot_enemy_1, robot_enemy_2;
+  list_of_enemy enemy_list, enemy_list_copy;
 
   gameover = 0;
 
   while (!gameover){
-  /* initialize SDL */
-  SDL_Init(SDL_INIT_VIDEO);
+    /* initialize SDL */
+    SDL_Init(SDL_INIT_VIDEO);
 
-  /* set the title bar */
-  SDL_WM_SetCaption("SDL Animation", "SDL Animation");
+    /* set the title bar */
+    SDL_WM_SetCaption("SDL Animation", "SDL Animation");
 
-  /* create window */
-  screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0);
+    /* create window */
+    screen = SDL_SetVideoMode(TAILLE_TUILE*NB_BLOCS_LARGEUR, TAILLE_TUILE*NB_BLOCS_HAUTEUR, 32,SDL_HWSURFACE|SDL_DOUBLEBUF);
 
-  /* set keyboard repeat */
-  SDL_EnableKeyRepeat(70, 70);
+    /* set keyboard repeat */
+    SDL_EnableKeyRepeat(70, 70);
 
-  /* load sprite */
-  temp = SDL_LoadBMP("sprite.bmp");
-  hero->sprite = SDL_DisplayFormat(temp);
-  SDL_FreeSurface(temp);
-
-  /* load background */ //A changer plus tard
-  switch(level){
-  case 1:
-    temp  = SDL_LoadBMP("background.bmp");
-    background = SDL_DisplayFormat(temp);
+    /* load sprite */
+    temp = SDL_LoadBMP("sprite.bmp");
+    hero->sprite = SDL_DisplayFormat(temp);
     SDL_FreeSurface(temp);
-    break;
-  case 2:
-    temp = SDL_LoadBMP("background2.bmp");
-    background = SDL_DisplayFormat(temp);
+   
+    //load tiles
+    temp = SDL_LoadBMP("tile0.bmp");
+    tile0 = SDL_DisplayFormat(temp);
     SDL_FreeSurface(temp);
-    break;
-    levelover = 0;
-  }
 
-  //load tiles
-  temp = SDL_LoadBMP("tile0.bmp");
-  tile0 = SDL_DisplayFormat(temp);
-  SDL_FreeSurface(temp);
+    temp = SDL_LoadBMP("tile1.bmp");
+    tile1 = SDL_DisplayFormat(temp);
+    SDL_FreeSurface(temp);
 
-  temp = SDL_LoadBMP("tile1.bmp");
-  tile1 = SDL_DisplayFormat(temp);
-  SDL_FreeSurface(temp);
+    temp = SDL_LoadBMP("tile2.bmp");
+    tile2 = SDL_DisplayFormat(temp);
+    SDL_FreeSurface;
 
-  temp = SDL_LoadBMP("tile2.bmp");
-  tile2 = SDL_DisplayFormat(temp);
-  SDL_FreeSurface;
+    temp = SDL_LoadBMP("tile3.bmp");
+    tile3 = SDL_DisplayFormat(temp);
+    SDL_FreeSurface;
 
-  temp = SDL_LoadBMP("tile3.bmp");
-  tile3 = SDL_DisplayFormat(temp);
-  SDL_FreeSurface;
+    /* create list of new enemy */
+    enemy_list = create_new_list_of_enemy();
+    robot_enemy_1 = create_new_enemy('R',screen, 250, 200, 0);
+    robot_enemy_2 = create_new_enemy('R',screen, 200, 200, 0);
+    enemy_list = cons(robot_enemy_1, enemy_list);
+    enemy_list = cons(robot_enemy_2, enemy_list);
 
-  /* create list of new enemy */
-  enemy_list = create_new_list_of_enemy();
-  robot_enemy = create_new_enemy('R',screen);
-  enemy_list = cons(robot_enemy, enemy_list);
-
-  /* setup sprite colorkey and turn on RLE */
-  hero->colorkey = SDL_MapRGB(screen->format, 0, 255, 255);
-  SDL_SetColorKey(hero->sprite, SDL_SRCCOLORKEY | SDL_RLEACCEL, hero->colorkey);
+    /* setup sprite colorkey and turn on RLE */
+    hero->colorkey = SDL_MapRGB(screen->format, 0, 255, 255);
+    SDL_SetColorKey(hero->sprite, SDL_SRCCOLORKEY | SDL_RLEACCEL, hero->colorkey);
   
-  /* set sprite position */
-  hero->coord.x = hero->x = 300;
-  hero->coord.y = hero->y = 335;
+    /* set sprite position */
+    hero->coord.x = hero->x = 300;
+    hero->coord.y = hero->y = 700;
 
-  /* set animation frame */
-  hero->rc_image.x = 0;
-  hero->rc_image.y = 0;
-  hero->rc_image.w = SPRITE_WIDTH;
-  hero->rc_image.h = SPRITE_HEIGHT;
+    /* set animation frame */
+    hero->rc_image.x = 0;
+    hero->rc_image.y = 0;
+    hero->rc_image.w = SPRITE_WIDTH;
+    hero->rc_image.h = SPRITE_HEIGHT;
 
-  /* Init camera (point of view) */
-  camera.h = SCREEN_HEIGHT;
-  camera.w = SCREEN_WIDTH;
-  camera.x = 0;
-  camera.y = 0;
+    levelover = 0;
 
-  levelover = 0;
+    char key[SDLK_LAST]= {0};
 
-  char key[SDLK_LAST]= {0};
+    char * table[][32] = { //16 blocs large * 12 blocs haut (64px sur 1024*768)
+      "0000000000000000",
+      "0000000002220000",
+      "0000000000000000",
+      "0002220000000000",
+      "0000000000000000",
+      "0000000022220000",
+      "0000000000000000",
+      "0000022000000000",
+      "0000222200000000",
+      "0002222220000000",
+      "0022222222000000",
+      "1111111111111111",
+    };
+   
 
-  /* message pump */
-  while (!levelover&&!gameover)
-    {
-      SDL_Event event;
+    /* message pump */
+    while (!levelover&&!gameover)
+      {
+	SDL_Event event;
 		
-      /* look for an event */
-
-      HandleEvent(key, screen);
-      update_events(key);
+	/* look for an event */
+	Afficher(screen, tile0, tile1, tile2, tile3, *table, NB_BLOCS_LARGEUR, NB_BLOCS_HAUTEUR);
+	HandleEvent(key, screen);
+	update_events(key);
       
-      jump(hero, SDL_GetTicks(), oldtime); // jumps only if oldtime < SDLGetTicks() so if u press up */
-      reload_pos(hero);
+	jump(hero, SDL_GetTicks(), oldtime); // jumps only if oldtime < SDLGetTicks() so if u press up */
+	reload_pos(hero);
 
-      /* collide with edges of screen */
-      if (hero->coord.x <= 0)
-	hero->coord.x = 0;
-      if (hero->coord.x >= SCREEN_WIDTH - SPRITE_WIDTH) 
-	hero->coord.x = SCREEN_WIDTH - SPRITE_WIDTH;
+	/* collide with edges of screen */
+	if (hero->coord.x <= 0)
+	  hero->coord.x = 0;
+	if (hero->coord.x >= SCREEN_WIDTH - SPRITE_WIDTH) 
+	  hero->coord.x = SCREEN_WIDTH - SPRITE_WIDTH;
 
-      if (hero->coord.y <= 0)
-	hero->coord.y = 0;
-      if (hero->coord.y >= SCREEN_HEIGHT - SPRITE_HEIGHT) 
-	hero->coord.y = SCREEN_HEIGHT - SPRITE_HEIGHT;
+	if (hero->coord.y <= 0)
+	  hero->coord.y = 0;
+	if (hero->coord.y >= SCREEN_HEIGHT - SPRITE_HEIGHT) 
+	  hero->coord.y = SCREEN_HEIGHT - SPRITE_HEIGHT;
 
-      // Next level when we are at the end of the screen
-      if (camera.x >= 2000-SCREEN_WIDTH){
-	levelover = 1;
-	camera.x = 0;
-	level ++;
+	/* draw the sprite */
+	SDL_BlitSurface(hero->sprite, &hero->rc_image, screen, &hero->coord);
+
+	/* draw the enemy sprite */
+	enemy_list_copy = enemy_list;
+	while (enemy_list_copy != NULL){
+	  SDL_BlitSurface(enemy_list_copy->first->sprite, &enemy_list_copy->first->rc_image, screen, &enemy_list_copy->first->coord);
+	  enemy_list_copy = enemy_list_copy->rest;
+	}
+
+	/* update the screen */
+	SDL_UpdateRect(screen, 0, 0, 0, 0);
       }
-      
-      SDL_BlitSurface(background, &camera, screen, NULL);
-
-      /* draw the sprite */
-      SDL_BlitSurface(hero->sprite, &hero->rc_image, screen, &hero->coord);
-
-      /* draw the enemy sprite */
-      SDL_BlitSurface(enemy_list->first->sprite, &enemy_list->first->rc_image, screen, &enemy_list->first->coord);
-
-      /* update the screen */
-      SDL_UpdateRect(screen, 0, 0, 0, 0);
-    }
   }
   /* clean up */
   SDL_FreeSurface(hero->sprite);
   SDL_FreeSurface(enemy_list->first->sprite);
-  SDL_FreeSurface(background);
+  SDL_FreeSurface(tile0);
+  SDL_FreeSurface(tile1);
+  SDL_FreeSurface(tile2);
+  SDL_FreeSurface(tile3);
   SDL_Quit();
 
   return 0;
